50d3a393368c4bc744aa4303669d9cf7ac8cc4d1
ShyroVO <bosquet.laurie@outlook.com>
2020-12-14 11:54:17 +0100

    1.0

--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,7 +1,7 @@
 let prenoms = ["pierre","paul","jacques"];
-    = "<Ton Prénom>";
+prenoms[0] = "Laurie";
 
-document.getElementById('viewport'). = ;
+document.getElementById('viewport').innerHTML = prenoms[0] ;
 
 
 
